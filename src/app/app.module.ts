7bf934f439041542ed93b74cc079ef514d5f2207
martinontcode <martin.onton@gmail.com>
2020-09-13 11:13:01 +0300

    User sidebar * Created sidebar front-end design for user component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,11 +12,8 @@ import { InMemoryDataService } from "./in-memory-data.service";
 import { ChatsComponent } from './chats/chats.component';
 import { ChatSettingsComponent } from './chats/chat-settings/chat-settings.component';
 import { ChatSearchComponent } from './chats/chat-search/chat-search.component';
-<<<<<<< HEAD
 import { UsersComponent } from './users/users.component';
-=======
 import { NavbarComponent } from './navbar/navbar.component';
->>>>>>> 96c0132ae03cb1b475488b8bf96f09b2f73ad48d
 
 @NgModule({
   declarations: [
@@ -25,11 +22,8 @@ import { NavbarComponent } from './navbar/navbar.component';
     ChatsComponent,
     ChatSettingsComponent,
     ChatSearchComponent,
-<<<<<<< HEAD
-    UsersComponent
-=======
+    UsersComponent,
     NavbarComponent
->>>>>>> 96c0132ae03cb1b475488b8bf96f09b2f73ad48d
   ],
   imports: [
     BrowserModule,
